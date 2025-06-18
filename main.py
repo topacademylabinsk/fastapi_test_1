3a111211777456b1c38e452b083e8d84d17b1c10
Anatoliy <cool.afkshnik@yandex.ru>
2025-06-18 17:40:07 +0300

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 templates = Jinja2Templates("./templates")
 
+
 @app.get("/")
 def root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
 
 @app.get("/index2")
 def index2(request: Request):
     return templates.TemplateResponse(request=request, name="index2.html")
 
+
 @app.get("/test_json")
 def test_json(request: Request):
-    return {"test":123}
+    return {"test": 123}
+
 
 if __name__ == "__main__":
     uvicorn.run(app=app, port=8555)
