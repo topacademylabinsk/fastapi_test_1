ffafeb1b3f1bd34429755b66d7b55732f9361a9b
Anatoliy <cool.afkshnik@yandex.ru>
2025-06-18 17:37:35 +0300

    Changes to be committed: 	modified:   main.py 	modified:   pyproject.toml 	new file:   static/style.css 	new file:   templates/index.html 	new file:   templates/index2.html 	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from fastapi-test-1!")
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+templates = Jinja2Templates("./templates")
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+@app.get("/index2")
+def index2(request: Request):
+    return templates.TemplateResponse(request=request, name="index2.html")
+
+@app.get("/test_json")
+def test_json(request: Request):
+    return {"test":123}
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app=app, port=8555)
